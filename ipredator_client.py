f5d6456c286a6ceb75c204e8e71b25b421554c8a
Jithesh <mail@jithe.sh>
2015-01-02 04:43:00 +0530

    Fixed some stray code in an else block and changed double quotes to single quotes

--- ipredator_client.py
+++ ipredator_client.py
@@ -15,7 +15,7 @@ The MIT License (MIT)
 Copyright (c) [2015] [Jithesh Eriyakkadan Janardhanan]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
+of this software and associated documentation files (the 'Software'), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
@@ -24,7 +24,7 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -34,6 +34,7 @@ SOFTWARE.
 '''
 
 import apt
+import sys
 import subprocess
 import argparse
 import shutil
@@ -41,21 +42,21 @@ import os.path
 
 def configure_client():
     cache = apt.Cache()
-    if cache["openvpn"].is_installed:
+    if cache['openvpn'].is_installed:
         # OpenVPN client is already installed! Let's not do anything and move out of the loop
         pass
     else:
         # Oopsie! OpenVPN client is not installed! Let's get on with it, shall we?
         try:
-            subprocess.check_call(["apt-get", "--assume-yes", "install", "openvpn"])
+            subprocess.check_call(['apt-get', '--assume-yes', 'install', 'openvpn'])
         except subprocess.CalledProcessError as e:
-            print "Installation failed:", e
+            print 'Installation failed:', e
 
-    parser = argparse.ArgumentParser(description = "Accepting source & destination arguments from commandline")
+    parser = argparse.ArgumentParser(description = 'Accepting source & destination arguments from commandline')
     parser.add_argument('source', metavar = 'SOURCE_PATH', help = 'full path to ipredator conf file')
-    parser.add_argument('-d', '--destination', metavar = 'DESTINATION_PATH: OPTIONAL', nargs = '?', default = '/etc/openvpn', help = "Directory to copy the conf file to. defaults to /etc/openvpn")
-    parser.add_argument('user', metavar = 'USERNAME', help = "IPredator username")
-    parser.add_argument('password', metavar = 'PASSWORD', help = "IPredator password")
+    parser.add_argument('-d', '--destination', metavar = 'DESTINATION_PATH: OPTIONAL', nargs = '?', default = '/etc/openvpn', help = 'Directory to copy the conf file to. defaults to /etc/openvpn')
+    parser.add_argument('user', metavar = 'USERNAME', help = 'IPredator username')
+    parser.add_argument('password', metavar = 'PASSWORD', help = 'IPredator password')
     args = parser.parse_args()
 
     # Copying the IPredator configuration file from source to destination
@@ -65,15 +66,16 @@ def configure_client():
     except IOError as e:
         print e
     if os.path.isfile(args.destination + '/update-resolv-conf'):
-        dns_conf = "script-security 2\nup /etc/openvpn/update-resolv-conf\ndown /etc/openvpn/update-resolv-conf"
+        dns_conf = 'script-security 2\nup /etc/openvpn/update-resolv-conf\ndown /etc/openvpn/update-resolv-conf'
         with open(args.destination + '/IPredator-CLI-Password.conf', 'a') as confFile:
             confFile.writelines(dns_conf)
     else:
-        raise
+        sys.exit('update-resolv-conf does not exist!')
+    
     # Creating /etc/openvpn/IPredator.auth file & appending credentials to it
     
     try:
-        subprocess.check_call(["touch", "/etc/openvpn/IPredator.auth"])
+        subprocess.check_call(['touch', '/etc/openvpn/IPredator.auth'])
     except subprocess.CalledProcessError as e:
         print e
 
@@ -81,10 +83,12 @@ def configure_client():
         authFile.write(args.user + '\n') 
         authFile.write(args.password)
 
+    # Changing the ownership and permissions on files
+    
     subprocess.call(['chown', 'root:root', '/etc/openvpn/IPredator-CLI-Password.conf'])
     subprocess.call(['chown', 'root:root', '/etc/openvpn/IPredator.auth'])
     subprocess.call(['chmod', '400', '/etc/openvpn/IPredator-CLI-Password.conf'])
     subprocess.call(['chmod', '400', '/etc/openvpn/IPredator.auth'])
     
-if __name__ == "__main__":
+if __name__ == '__main__':
     configure_client()
